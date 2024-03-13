d39113cb1c3f9127a6837093f5bd826f3803877d
Mira <mkgubareva2005@gmail.com>
2024-03-13 19:13:07 +0300

    :)

--- logger/client_logger/tests/client_logger_tests.cpp
+++ logger/client_logger/tests/client_logger_tests.cpp
@@ -4,30 +4,12 @@
 
 
 
-//int main(
-//    int argc,
-//    char *argv[])
-//{
-//    testing::InitGoogleTest(&argc, argv);
-//
-//    return RUN_ALL_TESTS();
-//}
-
-int main()
+int main(
+    int argc,
+    char *argv[])
 {
-    logger_builder* builder = new client_logger_builder();
+    testing::InitGoogleTest(&argc, argv);
 
-    logger* logger_1 = builder
-            ->add_file_stream("file_1.txt", logger::severity::trace)
-            ->add_console_stream(logger::severity::debug)
-            ->transform_with_configuration("json_logger.json", "logger")
-            ->build();
-
-    logger_1->debug("debug");
-    logger_1->trace("trace");
-
-    delete builder;
-    delete logger_1;
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
+
