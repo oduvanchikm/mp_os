f1622885b2d33db2858c50e0b7174edce6e2354a
Mira <mkgubareva2005@gmail.com>
2024-03-31 19:52:21 +0300

    constructor

--- allocator/allocator_sorted_list/include/allocator_sorted_list.h
+++ allocator/allocator_sorted_list/include/allocator_sorted_list.h
@@ -2,10 +2,13 @@
 #define MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_SORTED_LIST_H
 
 #include <allocator_guardant.h>
+#include "../../../logger/client_logger/include/client_logger.h"
 #include <allocator_test_utils.h>
 #include <allocator_with_fit_mode.h>
 #include <logger_guardant.h>
 #include <typename_holder.h>
+#include <mutex>
+#include <sstream>
 
 class allocator_sorted_list final:
     private allocator_guardant,
@@ -23,17 +26,13 @@ public:
 
     ~allocator_sorted_list() override;
 
-    allocator_sorted_list(
-        allocator_sorted_list const &other);
+    allocator_sorted_list(allocator_sorted_list const &other) = delete;
 
-    allocator_sorted_list &operator=(
-        allocator_sorted_list const &other);
+    allocator_sorted_list &operator=(allocator_sorted_list const &other) = delete;
 
-    allocator_sorted_list(
-        allocator_sorted_list &&other) noexcept;
+    allocator_sorted_list(allocator_sorted_list &&other) noexcept = delete;
 
-    allocator_sorted_list &operator=(
-        allocator_sorted_list &&other) noexcept;
+    allocator_sorted_list &operator=(allocator_sorted_list &&other) noexcept = delete;
 
 public:
 
@@ -54,8 +53,45 @@ public:
 
 public:
 
+//    inline void set_fit_mode(
+//        allocator_with_fit_mode::fit_mode mode) override;
+
+private:
+
+//    inline allocator *get_allocator() const override;
+
+public:
+
+//    std::vector<allocator_test_utils::block_info> get_blocks_info() const noexcept override;
+
+private:
+
+//    inline logger *get_logger() const override;
+
+//    inline std::string get_typename() const noexcept override;
+
+private:
+
+    size_t get_ancillary_space_size() const noexcept;
+
+    allocator_with_fit_mode::fit_mode get_fit_mode() const noexcept;
+
+    void* get_first_aviable_block() const noexcept;
+
+private:
+
+    allocator::block_size_t get_aviable_block_size(void* block_address) const noexcept;
+
+    static void* get_aviable_block_next_block_address(void* block_address) noexcept;
+
+    static block_size_t get_occupied_block_size(void* block_address) noexcept;
+//
+    std::mutex* get_mutex() const noexcept;
+
+public:
+
     inline void set_fit_mode(
-        allocator_with_fit_mode::fit_mode mode) override;
+            allocator_with_fit_mode::fit_mode mode) override;
 
 private:
 
--- allocator/allocator_sorted_list/src/allocator_sorted_list.cpp
+++ allocator/allocator_sorted_list/src/allocator_sorted_list.cpp
@@ -4,31 +4,63 @@
 
 allocator_sorted_list::~allocator_sorted_list()
 {
-    throw not_implemented("allocator_sorted_list::~allocator_sorted_list()", "your code should be here...");
+    auto* allocator = get_allocator();
+    auto* logger = get_logger();
+
+    if (allocator == nullptr)
+    {
+        ::operator delete(_trusted_memory);
+    }
+    else
+    {
+        allocator->deallocate(_trusted_memory);
+    }
+
+    if (logger != nullptr)
+    {
+        logger->trace("allocator is deleted");
+    }
 }
 
-allocator_sorted_list::allocator_sorted_list(
-    allocator_sorted_list const &other)
+size_t allocator_sorted_list::get_ancillary_space_size() const noexcept
 {
-    throw not_implemented("allocator_sorted_list::allocator_sorted_list(allocator_sorted_list const &)", "your code should be here...");
+    return sizeof(logger*) + sizeof(allocator*) + sizeof(size_t) + sizeof(void*) + sizeof(allocator_with_fit_mode::fit_mode) + sizeof(std::mutex*);
 }
 
-allocator_sorted_list &allocator_sorted_list::operator=(
-    allocator_sorted_list const &other)
+
+std::mutex* allocator_sorted_list::get_mutex() const noexcept
 {
-    throw not_implemented("allocator_sorted_list &allocator_sorted_list::operator=(allocator_sorted_list const &)", "your code should be here...");
+    return reinterpret_cast<std::mutex*>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*) + sizeof(logger*) + sizeof(size_t) + sizeof(allocator_with_fit_mode::fit_mode));
 }
 
-allocator_sorted_list::allocator_sorted_list(
-    allocator_sorted_list &&other) noexcept
+allocator_with_fit_mode::fit_mode allocator_sorted_list::get_fit_mode() const noexcept
 {
-    throw not_implemented("allocator_sorted_list::allocator_sorted_list(allocator_sorted_list &&) noexcept", "your code should be here...");
+    return *reinterpret_cast<allocator_with_fit_mode::fit_mode *>(reinterpret_cast<unsigned char *>(_trusted_memory) + sizeof(allocator *) + sizeof(logger *) + sizeof(size_t) + sizeof(size_t));
 }
 
-allocator_sorted_list &allocator_sorted_list::operator=(
-    allocator_sorted_list &&other) noexcept
+void *allocator_sorted_list::get_first_aviable_block() const noexcept
 {
-    throw not_implemented("allocator_sorted_list &allocator_sorted_list::operator=(allocator_sorted_list &&) noexcept", "your code should be here...");
+    return *reinterpret_cast<void **>(reinterpret_cast<unsigned char *>(_trusted_memory) + sizeof(allocator *) + sizeof(logger *) + sizeof(size_t) + sizeof(size_t) + sizeof(allocator_with_fit_mode::fit_mode) + sizeof(std::mutex*));
+}
+
+allocator::block_size_t allocator_sorted_list::get_aviable_block_size(void *block_address) const noexcept
+{
+    return *reinterpret_cast<size_t *>(block_address);
+}
+
+void *allocator_sorted_list::get_aviable_block_next_block_address(void *block_address) noexcept
+{
+    return *reinterpret_cast<void**>(reinterpret_cast<size_t *>(block_address) + 1);
+}
+
+allocator::block_size_t allocator_sorted_list::get_occupied_block_size(void *block_address) noexcept
+{
+    return *reinterpret_cast<allocator::block_size_t *>(block_address);
+}
+
+inline allocator *allocator_sorted_list::get_allocator() const
+{
+    return *reinterpret_cast<allocator**>(_trusted_memory);
 }
 
 allocator_sorted_list::allocator_sorted_list(
@@ -37,20 +69,206 @@ allocator_sorted_list::allocator_sorted_list(
     logger *logger,
     allocator_with_fit_mode::fit_mode allocate_fit_mode)
 {
-    throw not_implemented("allocator_sorted_list::allocator_sorted_list(size_t, allocator *, logger *, allocator_with_fit_mode::fit_mode)", "your code should be here...");
+
+    std::cout << "che za bred" << std::endl;
+//    class logger* log = get_logger();
+
+    std::cout << "WHERE???????????" << std::endl;
+
+
+
+    if (logger != nullptr)
+    {
+        logger->trace("ALLOCATOR_SORTED_LIST: the beginning of the constructor's work, allocator start creating");
+        std::cout << "ALLOCATOR_SORTED_LIST: the beginning of the constructor's work, allocator start creating" << std::endl;
+    }
+
+//    std::cout << "hello!" << std::endl;
+
+    if (space_size < sizeof(block_size_t) + sizeof(block_pointer_t))
+    {
+        if (logger != nullptr)
+        {
+            logger->error("ALLOCATOR_SORTED_LIST: can't initialize allocator instance");
+        }
+        throw std::logic_error("ALLOCATOR_SORTED_LIST: can't initialize allocator instance");
+    }
+
+    auto common_size = space_size + get_ancillary_space_size();
+
+    std::cout << "common size: " << common_size << std::endl;
+
+    try
+    {
+        _trusted_memory = parent_allocator == nullptr
+                          ? ::operator new(common_size)
+                          : parent_allocator->allocate(common_size, 1);
+    }
+    catch (std::bad_alloc const &ex)
+    {
+        logger->error("ALLOCATOR_SORTED_LIST: error with allocate memory");
+    }
+
+    allocator** parent_allocator_space_address = reinterpret_cast<allocator **>(_trusted_memory);
+    *parent_allocator_space_address = parent_allocator;
+
+    class logger** logger_space_address = reinterpret_cast<class logger**>(parent_allocator_space_address + 1);
+    *logger_space_address = logger;
+
+    size_t *space_size_space_address = reinterpret_cast<size_t *>(logger_space_address + 1);
+    *space_size_space_address = space_size;
+
+    size_t *size_memory = reinterpret_cast<size_t *>(space_size_space_address + 1);
+    *size_memory = space_size;
+
+    allocator_with_fit_mode::fit_mode *fit_mode_space_address = reinterpret_cast<allocator_with_fit_mode::fit_mode *>(size_memory + 1);
+    *fit_mode_space_address = allocate_fit_mode;
+
+    std::mutex** mutex_space_address = reinterpret_cast<std::mutex**>(fit_mode_space_address + 1);
+    auto* mutex_buddy_system = new std::mutex;
+    *mutex_space_address = mutex_buddy_system;
+
+    void **first_block_address_space_address = reinterpret_cast<void **>(mutex_space_address + 1);
+    *first_block_address_space_address = reinterpret_cast<void *>(first_block_address_space_address + 1);
+
+    *reinterpret_cast<size_t *>(*first_block_address_space_address) = space_size - sizeof(size_t) - sizeof(void*);
+    *reinterpret_cast<void** >(reinterpret_cast<size_t *>(*first_block_address_space_address) + 1) = nullptr;
+
+    if (logger != nullptr)
+    {
+        logger->trace("ALLOCATOR_SORTED_LIST: allocator is created")
+        ->information("ALLOCATOR_SORTED_LIST: size of memory allocated: " + std::to_string(space_size));
+        std::cout << "ALLOCATOR_SORTED_LIST: allocator has created" << std::endl;
+    }
 }
 
-[[nodiscard]] void *allocator_sorted_list::allocate(
-    size_t value_size,
-    size_t values_count)
+//size_t allocator_sorted_list::get_available_block_size(void* block_address) const noexcept
+//{
+//    if (block_address == nullptr)
+//    {
+//        std::cout << "nullptr" << std::endl;
+//    }
+//    return *reinterpret_cast<size_t*>(block_address);
+//}
+
+//void* allocator_sorted_list::get_available_block_next_block_address(void* block_address) noexcept
+//{
+//    return *reinterpret_cast<void**>(reinterpret_cast<unsigned char*>(block_address) + 1);
+//}
+//
+//allocator::block_size_t allocator_sorted_list::get_occupied_block_size(void* block_address) noexcept
+//{
+//    return *reinterpret_cast<allocator::block_size_t*>(block_address);
+//}
+
+//void* allocator_sorted_list::get_first_available_block() const noexcept
+//{
+//    return *reinterpret_cast<void**>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*) + sizeof(logger*) + sizeof(size_t) + sizeof(allocator_with_fit_mode::fit_mode) + sizeof(std::mutex*));
+//}
+
+[[nodiscard]] void *allocator_sorted_list::allocate(size_t value_size, size_t values_count)
 {
-    throw not_implemented("[[nodiscard]] void *allocator_sorted_list::allocate(size_t, size_t)", "your code should be here...");
+    logger* log = get_logger();
+
+    std::cout << "he" << std::endl;
+
+    if (log != nullptr)
+    {
+//        log->trace("ALLOCATOR_SORTED_LIST: method allocate has started");
+        std::cout << "ALLOCATOR_SORTED_LIST: method allocate has started" << std::endl;
+    }
+
+    auto requested_size = values_count * value_size;
+
+    std::cout << "requested size: " << requested_size << std::endl;
+
+    if (requested_size < sizeof(block_size_t) + sizeof(block_pointer_t))
+    {
+        requested_size = sizeof(block_size_t) + sizeof(block_pointer_t);
+        warning_with_guard("ALLOCATOR_SORTED_LIST: request space size was changed");
+    }
+
+    allocator_with_fit_mode::fit_mode fit_mode = get_fit_mode();
+
+    void* target_block = nullptr;
+    void* previous_to_target_block = nullptr;
+    void* next_to_target_block = nullptr;
+
+    {
+        void* previous_block = nullptr;
+        void* current_block = get_first_aviable_block();
+
+        while (current_block != nullptr)
+        {
+            size_t current_block_size = get_aviable_block_size(current_block);
+
+//            std::cout << "current block size: " << current_block_size << std::endl;
+
+            if (current_block_size >= requested_size &&
+                fit_mode == allocator_with_fit_mode::fit_mode::first_fit ||
+                fit_mode == allocator_with_fit_mode::fit_mode::the_best_fit &&
+                (target_block == nullptr ||
+                        get_aviable_block_size(target_block) > current_block_size) ||
+                fit_mode == allocator_with_fit_mode::fit_mode::the_worst_fit &&
+                (target_block == nullptr ||
+                        get_aviable_block_size(target_block) < current_block_size))
+            {
+                std::cout << "good" << std::endl;
+                previous_to_target_block = previous_block;
+                target_block = current_block;
+                next_to_target_block = get_aviable_block_next_block_address(current_block);
+            }
+
+            std::cout << "wtf" << std::endl;
+
+            previous_block = current_block;
+            current_block = get_aviable_block_next_block_address(current_block);
+        }
+    }
+
+    std::cout << ":):):):):))" << std::endl;
+
+    if (target_block == nullptr)
+    {
+        std::cout << "ALLOCATOR_SORTED_LIST: can't allocate" << std::endl;
+        error_with_guard("ALLOCATOR_SORTED_LIST: can't allocate");
+        throw std::bad_alloc();
+    }
+
+    size_t size_available_block = get_aviable_block_size(target_block);
+
+    std::cout << "size of target block: " << size_available_block << std::endl;
+
+    if (previous_to_target_block == nullptr)
+    {
+        auto blocks_sizes_difference = get_aviable_block_size(target_block) - requested_size;
+
+        if (blocks_sizes_difference > 0 && blocks_sizes_difference <  sizeof(block_pointer_t) + sizeof(block_size_t))
+        {
+            requested_size = get_aviable_block_size(target_block);
+            void* next_available_block = get_aviable_block_next_block_address(target_block);
+            void** first_available_block = reinterpret_cast<void**>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*) + sizeof(logger*) + sizeof(size_t) + sizeof(allocator_with_fit_mode::fit_mode) + sizeof(std::mutex));
+            *first_available_block = next_available_block;
+        }
+        else
+        {
+            size_t target_block_size = get_aviable_block_size(target_block);
+            // TODO: what is this????
+
+        }
+    }
+    else
+    {
+
+    }
+
+    return this;
 }
 
-void allocator_sorted_list::deallocate(
-    void *at)
+void allocator_sorted_list::deallocate(void *at)
 {
-    throw not_implemented("void allocator_sorted_list::deallocate(void *)", "your code should be here...");
+
+
 }
 
 inline void allocator_sorted_list::set_fit_mode(
@@ -59,19 +277,61 @@ inline void allocator_sorted_list::set_fit_mode(
     throw not_implemented("inline void allocator_sorted_list::set_fit_mode(allocator_with_fit_mode::fit_mode)", "your code should be here...");
 }
 
-inline allocator *allocator_sorted_list::get_allocator() const
-{
-    throw not_implemented("inline allocator *allocator_sorted_list::get_allocator() const", "your code should be here...");
-}
-
 std::vector<allocator_test_utils::block_info> allocator_sorted_list::get_blocks_info() const noexcept
 {
-    throw not_implemented("std::vector<allocator_test_utils::block_info> allocator_sorted_list::get_blocks_info() const noexcept", "your code should be here...");
+    logger* log = get_logger();
+    if (log != nullptr)
+    {
+        log->debug("ALLOCATOR_BUDDIES_SYSTEM: method get block info has started");
+        std::cout << "ALLOCATOR_BUDDIES_SYSTEM: method get block info has started" << std::endl;
+    }
+
+    std::vector<allocator_test_utils::block_info> data_vector_block_info;
+
+    void** l_current_block = reinterpret_cast<void**>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*) + sizeof(logger*) + sizeof(size_t) + sizeof(allocator_with_fit_mode::fit_mode) + sizeof(std::mutex*) + sizeof(size_t));
+    void* current_block = *l_current_block;
+
+    size_t current_size = 0;
+    size_t size_trusted_memory = 1 << *reinterpret_cast<size_t*>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*) + sizeof(logger*));
+
+    std::cout << size_trusted_memory << std::endl;
+
+    while (current_size < size_trusted_memory)
+    {
+        allocator_test_utils::block_info blocks_info;
+
+        blocks_info.is_block_occupied = *reinterpret_cast<unsigned char*>(current_block);
+
+        blocks_info.block_size =  1 << (*reinterpret_cast<short*>(reinterpret_cast<unsigned char*>(current_block) + 1));
+
+        data_vector_block_info.push_back(blocks_info);
+        current_block = get_aviable_block_next_block_address(current_block);
+
+        current_size += blocks_info.block_size;
+    }
+
+    std::string data_str;
+
+    for (block_info value : data_vector_block_info)
+    {
+        std::string is_oc = value.is_block_occupied ? "YES" : "NO";
+        data_str += (is_oc + "  " + std::to_string(value.block_size) + " | ");
+    }
+
+    log_with_guard("ALLOCATOR_BUDDIES_SYSTEM: state blocks: " + data_str, logger::severity::debug);
+    std::cout << "ALLOCATOR_BUDDIES_SYSTEM: state block " << data_str << std::endl;
+
+    if (log != nullptr)
+    {
+        log->debug("ALLOCATOR_BUDDIES_SYSTEM: method get block info has finished");
+    }
+
+    return data_vector_block_info;
 }
 
 inline logger *allocator_sorted_list::get_logger() const
 {
-    throw not_implemented("inline logger *allocator_sorted_list::get_logger() const", "your code should be here...");
+    return *reinterpret_cast<logger **>(reinterpret_cast<unsigned char*>(_trusted_memory) + sizeof(allocator*));
 }
 
 inline std::string allocator_sorted_list::get_typename() const noexcept
